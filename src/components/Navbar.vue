5e230d28daad9f764998b6b0e62457352a069918
Ben Ward <benward83@gmail.com>
2020-12-22 11:42:52 +0100

    Improved navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,22 @@
   <div class="header">
     <b-navbar variant="dark" type="light" class="bg-dark navbar-dark">
       <b-navbar-brand tag="h1" class="mb-0">Mr Ben's Records</b-navbar-brand>
+
+      <b-navbar-toggle target="navbar-toggle-collapse">
+      <template #default="{ expanded }">
+        <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+        <b-icon v-else icon="chevron-bar-down"></b-icon>
+      </template>
+    </b-navbar-toggle>
+
+    <b-collapse id="navbar-toggle-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">12" singles</b-nav-item>
+        <b-nav-item href="#">7" singles</b-nav-item>
+        <b-nav-item href="#">Beats</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+
     </b-navbar>
   </div>
 </template>
