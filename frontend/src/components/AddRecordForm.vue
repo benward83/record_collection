730d29b688482591b57f7833f7b962fb5864f5b1
Benjamin Ward <benward83@gmail.com>
2021-03-05 17:34:49 +0100

    Continuation of AddRecordForm design

--- frontend/src/components/AddRecordForm.vue
+++ frontend/src/components/AddRecordForm.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="background">
+  <div class="form-background">
     <div class="container mt-3">
       <form @submit.prevent="submit" class="form-control" action="">
-        <h4>Add a record</h4>
+        <h4 class="form-title">Add a record</h4>
         <div class="form-input">
           <b-form-input
             class="mt-2"
@@ -34,22 +34,72 @@
         <div class="dropdown mt-3">
           <b-form-select v-model="selected" :options="options"></b-form-select>
           <!-- radio btns -->
-          <div class='radio-btns mt-3'>
+          <div class="radio-btns mt-3">
             <h6>Condition of the record</h6>
-            <b-form-radio-group v-slot="{ ariaDescribedby }">
-              <b-form-radio
-                v-model="recordQuality"
-                :aria-describedby="ariaDescribedby"
-                name="some-radios"
-                value="A"
-              >Mint</b-form-radio>
-              <b-form-radio
-                v-model="recordQuality"
-                :aria-describedby="ariaDescribedby"
-                name="some-radios"
-                value="B"
-              >VG</b-form-radio>
-            </b-form-radio-group>
+            <div class="form-check form-check-inline">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio1"
+                value="option1"
+              />
+              <label class="form-check-label" for="inlineRadio1">M</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio2"
+                value="option2"
+              />
+              <label class="form-check-label" for="inlineRadio2">NM</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio3"
+                value="option3"
+              />
+              <label class="form-check-label" for="inlineRadio3"
+                >VG+</label
+              >
+            </div>
+            <div class="form-check form-check-inline">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio4"
+                value="option4"
+              />
+              <label class="form-check-label" for="inlineRadio1">VG</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio5"
+                value="option5"
+              />
+              <label class="form-check-label" for="inlineRadio2">G</label>
+            </div>
+            <div class="form-check form-check-inline">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+                id="inlineRadio6"
+                value="option6"
+              />
+              <label class="form-check-label" for="inlineRadio6"
+                >P</label
+              >
+            </div>
           </div>
         </div>
         <div class="rating mt-3">
@@ -58,15 +108,10 @@
           </b-form-rating>
         </div>
         <div class="form-btns mt-3">
-          <b-button
-           @click="$router.push('/')"
-            class="cancel"
-            variant="danger"
-          >Cancel</b-button>
-          <b-button
-            class="submit"
-            variant="success"
-          >Submit</b-button>
+          <b-button @click="$router.push('/')" class="cancel" variant="danger"
+            >Cancel</b-button
+          >
+          <b-button class="submit" variant="success">Submit</b-button>
         </div>
       </form>
     </div>
@@ -89,20 +134,13 @@ export default {
         { value: 'b', text: 'World' },
         { value: 'a', text: 'Jazz' },
       ],
-      recordQuality: '',
-      chosen: 'first',
-      choices: [
-        { text: 'First radio', value: 'first' },
-        { text: 'Second radio', value: 'second' },
-        { text: 'Third radio', value: 'third' },
-      ],
     };
   },
 };
 </script>
 
 <style scoped>
-.background {
+.form-background {
   position: absolute;
   top: 0;
   left: 0;
@@ -114,17 +152,18 @@ export default {
   opacity: 0.8;
 }
 
+.form-control {
+  display: inline-block;
+  margin: auto auto;
+}
+
 .container {
-  padding: 10px, 10px;
+  display: block;
+  text-align: center;
   justify-content: center;
   border-radius: 18px 18px 18px 18px;
   max-width: 50%;
   height: 30%;
-}
-
-h4 {
-  text-align: center;
-  color: rgb(95, 0, 0);
 }
 
 .form-btns {
@@ -141,5 +180,10 @@ h4 {
 .submit {
   float: right;
   margin-left: 15px;
+}
+.form-title {
+  text-align: center;
+  color: rgb(95, 0, 0);
+  padding: 10px 10px
 }
 </style>
